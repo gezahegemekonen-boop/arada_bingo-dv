fc8f5a450637edcab21e281eaa267aecdcd74629
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-15 05:12:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -182,7 +182,7 @@ async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     await update.callback_query.edit_message_text(text)
 
-async def invite(update: Update, context:
+async def invite(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
      async def invite(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.callback_query.answer()
