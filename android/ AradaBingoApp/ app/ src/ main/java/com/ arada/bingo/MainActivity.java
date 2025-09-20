fcdf8c3c7d32b3e8e74776df7cae0a798a163a88
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-20 07:22:44 -0700

    Create  MainActivity.java

---
+++ android/ AradaBingoApp/ app/ src/ main/java/com/ arada/bingo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.arada.bingo;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    WebView webView = new WebView(this);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.setWebViewClient(new WebViewClient());
+    webView.loadUrl("https://arada-bingo.et/cartela-editor?id=123456789");
+    setContentView(webView);
+  }
+}
