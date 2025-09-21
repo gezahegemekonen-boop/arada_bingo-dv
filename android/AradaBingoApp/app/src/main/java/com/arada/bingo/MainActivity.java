ef0568a4ba593f9182cdd35b4a898398116d6104
gezahegemekonen-boop <gezahegemekonen@gmail.com>
2025-09-22 01:02:47 +0300

    Update MainActivity.java

--- android/AradaBingoApp/app/src/main/java/com/arada/bingo/MainActivity.java
+++ android/AradaBingoApp/app/src/main/java/com/arada/bingo/MainActivity.java
@@ -1,1 +1,19 @@
+package com.arada.bingo;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://arada-bingo-dv-oxct.onrender.com");
+        setContentView(webView);
+    }
+}
 
